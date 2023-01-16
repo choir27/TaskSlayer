8ff482f3b001893b0fb9e844787d8999d49869b8
Richard Choi <richardchoi54@gmail.com>
2023-01-16 18:48:34 -0500

    remove ensureAuth from backend to reduce redunancy

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const authController = require("../controllers/user");
 const MongoClient = require('mongodb').MongoClient
 require("dotenv").config();
-const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 let db,
 dbName = 'test'
@@ -16,7 +15,7 @@ MongoClient.connect(process.env.MONGO_URI, { useUnifiedTopology: true })
 
 //Main Routes - simplified for now
 router.post("/login", authController.postLogin);
-router.get("/logout", ensureAuth, authController.logout);
+router.get("/logout", authController.logout);
 router.post("/register", authController.postSignup);
 router.get('/api',(req,res)=>{
     db.collection('users').find().toArray()
