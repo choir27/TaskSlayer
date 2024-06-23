3e6785d0d1e0f8c298763474126432c74fffc95e
choir27 <richardchoi54@gmail.com>
2024-06-23 16:00:01 -0400

    appwrite config setup

---
+++ api/config.tsx
@@ -0,0 +1,10 @@
+import { Client, Account } from 'appwrite';
+
+export const client = new Client();
+
+client
+    .setEndpoint(process.env.NEXT_PUBLIC_ENDPOINT)
+    .setProject(process.env.NEXT_PUBLIC_PROJECT_ID);
+
+export const account = new Account(client);
+export { ID } from 'appwrite';
