d4f1f08a39c16e41e8f6eddb29f004492df97f30
choir27 <richardchoi54@gmail.com>
2024-06-30 09:02:14 -0400

    change folder location for button stories, button component and button styling and update .storybook/main.ts

--- components/Button/Button.stories.ts
+++ components/Button/Button.stories.ts
@@ -10,7 +10,6 @@ const meta = {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
@@ -26,7 +25,7 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
+    variant: 'primary',
     label: 'Button',
   },
 };
